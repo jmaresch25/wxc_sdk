e7b9c76876599c98597a54db1808f144b764a078
Kpain <136159002+K8pain@users.noreply.github.com>
2026-02-12 04:07:55 +0100

    Delete SpaceOdT directory

--- SpaceOdT/__init__.py
+++
@@ -1,5 +0,0 @@
-"""SpaceOdT package."""
-
-from .export_runner import run_export
-
-__all__ = ["run_export"]
--- SpaceOdT/modules/people.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from typing import Any
-
-from .common import safe_iterable_rows
-
-
-def collect_people(api: Any) -> list[dict[str, Any]]:
-    return safe_iterable_rows(api.people.list())
